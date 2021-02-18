927ee875c8d14e5a9b8758d9894a7376845a7358
乔春晓 <>
2021-02-18 13:15:39 +0800

    创建版本

---
+++ EmayLogin/EmayLogin/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  EmayLogin
+//
+//  Created by 乔春晓 on 2021/2/18.
+//
+
+#import "ViewController.h"
+#import "UniLogin/UniLogin.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
